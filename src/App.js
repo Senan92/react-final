1c6496bd31204ce4e19a755cabae240265cb63c3
Senan Stenhav <senan.stenhav@hotmail.com>
2018-10-18 13:25:21 +0200

    updated app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
 
-export default App;
+    constructor(props) {
+      super(props);
+      this.state = {
+        isLoaded: false,
+        items: []
+      }
+    }
+
+    componentDidMount() {
+      
+
+      const myHeaders = new Headers();
+
+      myHeaders.append('x-api-key', 'b2ac60d8-6e21-41b1-980f-33ec13134ed0');
+      myHeaders.append('Content-Type', 'application/json');
+      myHeaders.append('cache-control', 'no-cache')
+
+      fetch('http://testing.euw.istaffsystems.com', {
+        method: 'GET',
+        headers: myHeaders,
+        async: true,
+      })
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json,
+        })
+      });
+    }
+    
+render() {
+      var{isLoaded, items} = this.state;
+      if(!isLoaded) {
+        return <div>Loading...</div>;
+      }
+      
+      else{
+       return (
+         <div className="App">
+           <ul>
+             {items.map(item => (
+               <li key={item.id}>
+                 Name: {item.name} | Id:{item.id}
+               </li>
+             ))};
+           </ul>
+         </div>
+       );
+      }
+    }
+  }
+  
+  export default App;
+  
